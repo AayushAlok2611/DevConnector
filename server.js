d79e5038c804fcaf1efccd084be9fc88d994f2e3
AayushAlok <59266619+AayushAlok@users.noreply.github.com>
2021-08-03 16:23:51 +0530

    commit

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const connectDB = require("./config/db.js");
-
+const path = require('path');
 const app = express();
 
 //Init middleware to parse req.body
@@ -14,10 +14,15 @@ app.use('/api/auth',require('./Routes/api/auth'));
 app.use('/api/posts',require('./Routes/api/posts'));
 app.use('/api/profile',require('./Routes/api/profile'));
 
-
-app.get('/', (req, res) => {
-    res.send('API running');
-})
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
 
 const PORT = process.env.PORT || 5000; //if no env varuable set defaults to port no 5000
 
